afeeeb652b2a9d973c194e1cec411b1f47930ddd
Jesús Corvo <44329031+Mastercorvo@users.noreply.github.com>
2020-11-26 21:27:27 -0400

    Añadiendo Cache de Resgistros

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 
 import './App.css';
 
-import { useState } from 'react';
+import { useState, useEffect, useRef } from 'react';
 
 import { v4 as uuidv4 } from 'uuid';
 
@@ -10,25 +10,71 @@ const REGISTERS = {
     "Primero":{
       name: "Andres Olivera",
       equipo: "Televisor Samsung 14",
-      create_at_date: new Date().toDateString(),
+      create_at_date: getNowTime(),
       phones:[{number:"000000", name:"Casa"}, {number:"111111", name:"Personal"}],
       title: "No prende",
       description: "dopfijkdpgjkerpgk509ti0rgkdfpkjweoprp34kfmp0edk39rfkigj349",
-      arreglado: "No arreglado",
+      fix: false,
       pendiente: true,
-      note: "Faltan piezas",
+      cache: [{ name: "Andres Olivera",
+      equipo: "Televisor Samsung 14",
+      phones:[{number:"000000", name:"Casa"}, {number:"111111", name:"Personal"}],
+      title: "No prende",
+      description: "dopfijkdpgjkerpgk509ti0rgkdfpkjweoprp34kfmp0edk39rfkigj349",
+      fix: false,
+      pendiente: true,
+      MODIFY_AT_DATE: getNowTime()}]
     }
 }
-function Register({name: NAME, equipo: EQUIPO, create_at_date: CREATE_AT_DATE, phones: PHONES, title: TITLE, description: DESCRIPTION, fix: FIX, pendiente: PENDIENTE, id}){
+
+function getNowTime(){
+
+  const DATE = new Date();
+
+  return `${DATE.getDate()}/${DATE.getMonth()}/${DATE.getFullYear()} - Hora ${DATE.getHours()} Minuto ${DATE.getMinutes()} Segundos ${DATE.getSeconds()}`
+
+}
+
+function Cache({DATA, setShowCache}){
+
+  function buttonHandler(){
+
+    setShowCache(false)
+
+  }
+
+  const SCROLL = useRef()
+
+  useEffect(()=>{
+
+    SCROLL.current.scroll(0, 0);
+
+  },[]);
+
+  return (<div className="cache">
+
+    <div className="container" ref={SCROLL}>
+
+      <h2>Registros</h2>
+
+    {DATA.map(e=><p>Actualización Hecha El: {e.MODIFY_AT_DATE} <br/> Nombre: {e.name} - Titulo: {e.title} - Equipo: {e.equipo} <br/> Descripción: {e.description} <br/> Reparado: {e.fix?'Sí.':'No.'} - Entregado: {e.pendiente?'Sí.':'No.'}</p>)}
+
+    </div>
+    <button onClick={buttonHandler}>Salir</button>
+
+  </div>)
+
+}
+
+function Register(DATA){
+
+  const {name: NAME, equipo: EQUIPO, create_at_date, phones: PHONES, title: TITLE, description: DESCRIPTION, fix: FIX, pendiente: PENDIENTE, id, setRegisters, cache} = DATA;
 
   const [name, setName] = useState(NAME);
   const nameHandler = ({target})=> setName(target.value); 
   const [equipo, setEquipo] = useState(EQUIPO);
   const equipoHandler = ({target})=> setEquipo(target.value); 
-  const [create_at_date, setCreate_at_date] = useState(CREATE_AT_DATE || new Date().toDateString());
-  const create_at_dateHandler = ({target})=> setCreate_at_date(target.value); 
   const [phones, setPhones] = useState(PHONES);
-  const phonesHandler = ({target})=> setPhones(target.value); 
   const [title, setTitle] = useState(TITLE);
   const titleHandler = ({target})=> setTitle(target.value); 
   const [description, setDescription] = useState(DESCRIPTION);
@@ -42,21 +88,96 @@ function Register({name: NAME, equipo: EQUIPO, create_at_date: CREATE_AT_DATE, p
   const [numberPhone, setNumberPhone] = useState('');
   const numberPhoneHandler = ({target})=> setNumberPhone(target.value); 
 
+  const [deletedPhones, setDeletedPhones] = useState(false);
+  const deletePhonesHandler = ()=> setDeletedPhones(v=>!v);
+
+  const [showCache, setShowCache] = useState(false)
+
+  const [edit, setEdit] = useState(false);
+
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  useEffect(()=>{
+
+    const objetData = JSON.stringify({
+      name, equipo, create_at_date, phones, title, description, fix, pendiente
+    })
+    
+
+    const COPY = {...DATA}
+
+    delete COPY.id
+    delete COPY.cache
+
+    const OriginalObject = JSON.stringify(COPY);
+
+    if(OriginalObject !== objetData) setEdit(true);
+      else setEdit(false)
+
+  })
+
+  function addPhone(){
+
+    if(!numberPhone || !personPhone) return;
+
+    if(phones.find(({number})=> number === numberPhone)) return alert('Numero de Teléfono Ya Registrado');
+
+    setPhones(value=>[{number: numberPhone, name: personPhone}, ...value, ]);
+
+    setNumberPhone('');
+
+    setPersonPhone('');
+
+  }
+
+  function deletePhone(phone){
+
+    setPhones(value=>{
+
+      return value.filter(({number})=> number !== phone)
+
+    })
+
+  }
+
+  function saveHandler(){
+
+    setRegisters(value=>{
+
+      const COPY = {...value};
+
+      const OLD_OBJECT = {name, equipo, phones, title, description, fix, pendiente, cache, MODIFY_AT_DATE: getNowTime()}
+      
+      console.log(OLD_OBJECT);
+      COPY[id] = {name, equipo, create_at_date, phones, title, description, fix, pendiente, cache :[OLD_OBJECT ,...cache]};
+
+
+      return COPY;
+
+    })
+
+  }
+
   return (<div className="register">
+    {showCache && <Cache DATA={DATA.cache} setShowCache={setShowCache}/>}
+    <div className="save" onClick={saveHandler} style={{display:edit?'block':'none'}}></div>
     <div className="title-container">
       <label>Titulo: </label><input type="text" className="title" placeholder="Titulo de la Falla" value={title} onChange={titleHandler} />
       <label>De: </label>
       <input type="text" placeholder="Persona" value={name} onChange={nameHandler} />
     </div>
-    <input type="text" className="equipo" placeholder="Equipo" value={equipo} onChange={equipoHandler} />
-    <textarea className="description" placeholder="Description" value={description} onChange={descriptionHandler} />
+    <label>Equipo: <input type="text" className="equipo" placeholder="Equipo" value={equipo} onChange={equipoHandler} /></label>
+    <div className="container">
+      <textarea className="description" placeholder="Description" value={description} onChange={descriptionHandler} />
+      <button onClick={()=>setShowCache(true)}>📜 Ver Historial</button>
+    </div>
     <div className="phones">
       <div className="container">
         <input type="text" placeholder="Teléfono" value={numberPhone} onChange={numberPhoneHandler} />
         <input type="text" placeholder="Persona" value={personPhone} onChange={personPhoneHandler} />
-        <button>+ Añadir Teléfono</button>
+        <button onClick={addPhone}>+ Añadir Teléfono</button>
+        <button onClick={deletePhonesHandler} className="delete">❌</button>
       </div>
-      {phones.map(({number, name}, i)=> <p className="phone" key={i}>{number} - {name}</p>)}
+{phones.map(({number, name}, i)=> <div className="phone" key={i}>{deletedPhones && <button onClick={()=>deletePhone(number)}>❌</button>} {number} - {name} </div>)}
     </div>
     <div className="estado">
       <div className="container">
@@ -64,8 +185,7 @@ function Register({name: NAME, equipo: EQUIPO, create_at_date: CREATE_AT_DATE, p
         <button style={{backgroundColor: pendiente? 'gold': 'green'}} onClick={pendienteHandler}>{pendiente?'📦 No Entregado': '✅ Entregado'}</button>
 
       </div>
-      <p className="date">{create_at_date}</p>
-
+      <p className="date">Solicitado El: {create_at_date}</p>
     </div>
 
   </div>);
@@ -73,6 +193,9 @@ function Register({name: NAME, equipo: EQUIPO, create_at_date: CREATE_AT_DATE, p
 }
 
 function App() {
+
+  const [registers, setRegisters] = useState(REGISTERS)
+
   return (
     <div className="App">
 
@@ -84,7 +207,7 @@ function App() {
 
       <div className="registers">
 
-        {Object.entries(REGISTERS).map(([index, value])=> <Register {...value} />)}
+        {Object.entries(registers).map(([index, value])=> <Register {...value} setRegisters={setRegisters} id={index}/>)}
 
       </div>
 
